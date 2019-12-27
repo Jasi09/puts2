943cb061fc806a3b1f53cdb714f68ae5b69177f8
Jesse Mac <jesse.lagah@gmail.com>
2019-12-27 15:08:26 +0100

    Added fraction to handle integer and rational number

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, request
+from fractions import Fraction
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    print('Usage ADD')
+   # print("Usage;\n<Operation>?A=<Value1>&B=<Value2>\n")
+    #return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+    value1=request.args.get('A',default = 0)
+    value2=request.args.get('B',default = 0)
+    result=Fraction(value1)+Fraction(value2)
+    return '%s \n' % result
 
 
 if __name__ == "__main__":
